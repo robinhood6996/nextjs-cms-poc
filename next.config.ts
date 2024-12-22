6ebd828b018878d54548b123859673b21cf4264d
Fakrul Islam Robin <fakrulrobin7@gmail.com>
2024-12-22 22:03:08 +0600

    deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "",
+  basePath: "my-headless-cms",
 };
 
 export default nextConfig;
